a1da0a65bd6bfba9c695b53b53a497808fa6169d
Ajay Seemar <ajseemar@gmail.com>
2019-08-27 01:55:16 -0700

    add token validation for slack bot and return test image

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -6,9 +7,43 @@ app = Flask(__name__)
 def index():
     return 'Hello World from Flask App'
 
-@app.route('/parse-bot')
+@app.route('/parse-bot', methods=["POST"])
 def parse():
-    return 'Responding to slash command...'
+    token = request.form.get('token', None)  # TODO: validate the token
+    if token != os.environ.get('SLACK_BOT_TOKEN'):
+        print('invalid token')
+        return
+    command = request.form.get('command', None)
+    text = request.form.get('text', None)
+    print(command)
+    print(text)
+    print()
+    print(request.form)
+    # return {
+	# 	"type": "section",
+	# 	"text": {
+	# 		"type": "mrkdwn",
+	# 		"text": "Take a look at this image."
+	# 	},
+	# 	"accessory": {
+	# 		"type": "image",
+	# 		"image_url": "https://image.shutterstock.com/image-photo/funny-cat-ophthalmologist-appointmet-squinting-260nw-598805597.jpg",
+	# 		"alt_text": "palm tree"
+	# 	}
+	# }
+
+    return {
+		"type": "image",
+		"title": {
+			"type": "plain_text",
+			"text": "Example Image",
+			"emoji": true
+		},
+		"image_url": "https://image.shutterstock.com/image-photo/funny-cat-ophthalmologist-appointmet-squinting-260nw-598805597.jpg",
+		"alt_text": "Example Image"
+	}
+
+    # return 'Responding to slash command...'
     # return 'Retrieving requested HTML content...'
 
 
