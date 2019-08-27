97bba0aa1a5a9759274bf930aa8d90802a6da974
Ajay Seemar <ajseemar@gmail.com>
2019-08-27 02:23:08 -0700

    add test method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 import os
 from config import VERIFICATION_TOKEN as VT
 import pdb
@@ -10,6 +10,53 @@ def index():
     return 'Hello World from Flask App'
 
 @app.route('/parse-bot', methods=["POST"])
+def slash_command():
+    """Parse the command parameters, validate them, and respond.
+    Note: This URL must support HTTPS and serve a valid SSL certificate.
+    """
+    # Parse the parameters you need
+    token = request.form.get('token', None)  # TODO: validate the token
+    command = request.form.get('command', None)
+    text = request.form.get('text', None)
+    print(command)
+    print(text)
+    # Validate the request parameters
+    if not token:  # or some other failure condition
+        abort(400)
+    # Use one of the following return statements
+    # 1. Return plain text
+    return 'Simple plain response to the slash command received'
+    # 2. Return a JSON payload
+    # See https://api.slack.com/docs/formatting and
+    # https://api.slack.com/docs/attachments to send richly formatted messages
+    return jsonify({
+        # Uncomment the line below for the response to be visible to everyone
+        # 'response_type': 'in_channel',
+        'text': 'More fleshed out response to the slash command',
+        'attachments': [
+            {
+                'fallback': 'Required plain-text summary of the attachment.',
+                'color': '#36a64f',
+                'pretext': 'Optional text above the attachment block',
+                'author_name': 'Bobby Tables',
+                'author_link': 'http://flickr.com/bobby/',
+                'author_icon': 'http://flickr.com/icons/bobby.jpg',
+                'title': 'Slack API Documentation',
+                'title_link': 'https://api.slack.com/',
+                'text': 'Optional text that appears within the attachment',
+                'fields': [
+                    {
+                        'title': 'Priority',
+                        'value': 'High',
+                        'short': False
+                    }
+                ],
+                'image_url': 'http://my-website.com/path/to/image.jpg',
+                'thumb_url': 'http://example.com/path/to/thumb.png'
+            }
+        ]
+    })
+
 def parse():
     token = request.form.get('token', None)  # TODO: validate the token
     pdb.set_trace()
