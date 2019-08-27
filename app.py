fcb12c63a5d7c518bc1a9c3954528d051e9a2e62
Ajay Seemar <ajseemar@gmail.com>
2019-08-27 14:17:25 -0700

    remove commented code

--- app.py
+++ app.py
@@ -11,12 +11,7 @@ def index():
 @app.route('/parse', methods=["POST"])
 def parse():
     # token = request.form.get('token', None)  # TODO: validate the token
-    # print('token: {}'.format(token))
-    # pdb.set_trace()
-    # if token != VT:
-    #     print('invalid token')
-    #     return
-    # command = request.form.get('command', None)
+
     text = request.form.get('text', None).split(" ")
     if len(text) != 3:
         image_url = "https://pbs.twimg.com/media/CuLpTqtWYAIiu1l.jpg"
@@ -33,18 +28,15 @@ def parse():
     tag = text[1]
     class_or_id = text[2].split("=")
 
-    # source = requests.get(website).text
     source = urllib.request.urlopen(website)
     soup = BeautifulSoup(source, 'lxml')
 
-    # element = None
     if class_or_id[0] == 'class':
         element = soup.find(tag, class_=class_or_id[1])
     else:
         element = soup.select("#{}".format(class_or_id[1]))[0]
         return element['src']
 
-    # if tag == 'img':
     image_url = element['src']
     attachments = [{"title": "", "image_url": image_url}]
     payload = {
@@ -53,50 +45,6 @@ def parse():
         'attachments': attachments
     }
     return jsonify(payload)
-
-        
-        
-    # else:
-    #     msg = element.text
-    #     payload = {
-    #         'response_type': 'in_channel',
-    #         'text': msg,
-    #     }
-    #     return jsonify(payload)
-
-    # print("command: {}".format(command))
-    # print("text: {}".format(text))
-    # print()
-    # print(request.form)
-    # return {
-	# 	"type": "section",
-	# 	"text": {
-	# 		"type": "mrkdwn",
-	# 		"text": "Take a look at this image."
-	# 	},
-	# 	"accessory": {
-	# 		"type": "image",
-	# 		"image_url": "https://image.shutterstock.com/image-photo/funny-cat-ophthalmologist-appointmet-squinting-260nw-598805597.jpg",
-	# 		"alt_text": "palm tree"
-	# 	}
-	# }
-    
-    
-    # payload = {
-	# 	"type": "image",
-	# 	"title": {
-	# 		"type": "plain_text",
-	# 		"text": "image1",
-	# 		"emoji": True
-	# 	},
-	# 	"image_url": "https://api.slack.com/img/blocks/bkb_template_images/beagle.png",
-	# 	"alt_text": "image1"
-	# }
-
-    
-
-    # return 'Responding to slash command...'
-    # return 'Retrieving requested HTML content...'
 
 
 if __name__ == '__main__':
